0fba98b51c31d58cb2e81600a2f4670f2678f2b0
Aleksandr Kovalyov <bahx4pc@gmail.com>
2022-10-20 00:55:36 +0200

    Dystrybucja 1 – opytmalizacja wstecz

--- src/main/java/pwo/seq/FibonacciGenerator.java
+++ src/main/java/pwo/seq/FibonacciGenerator.java
@@ -37,9 +37,7 @@ public class FibonacciGenerator extends Generator {
         } else {
             current = new BigDecimal(0);
         }
-
         lastIndex++;
-
         return current;
     }
 }
